8d9ed5d422e6d9d0b079d54e9b7a25dd0f12f81e
David Liebana <david.liebana@wallbox.com>
2024-09-13 10:21:36 +0200

    feat(button): add button variant prop to create seocndary buttons if needed

--- app/src/components/button/index.tsx
+++ app/src/components/button/index.tsx
@@ -2,22 +2,31 @@ import type { ReactNode } from 'react';
 import { StyledButton, StyledLink } from './styles';
 
 interface ButtonProps {
+  variant?: 'primary' | 'secondary'
   onClick?: () => void;
   to?: string
   'data-testid'?: string
   children: ReactNode;
 }
 
-function Button({ onClick, children, className, to, "data-testid": dataTestId }: ButtonProps & { className?: string }) {
+function Button({ onClick, children, className, to, variant= 'primary', "data-testid": dataTestId }: ButtonProps & { className?: string }) {
   
   const isButton = <StyledButton
     data-testid={dataTestId}
+    variant={variant}
     onClick={onClick}
   >
     {children}
   </StyledButton>,
 
-   isLink = <StyledLink data-testid={dataTestId} className={className} href={to} target='_blank' rel="noopener noreferrer">
+   isLink = <StyledLink 
+     data-testid={dataTestId}
+     variant={variant}
+     className={className}
+     href={to}
+     target='_blank'
+     rel="noopener noreferrer"
+    >
     {children}
   </StyledLink>
   
--- app/src/components/button/styles.tsx
+++ app/src/components/button/styles.tsx
@@ -1,16 +1,35 @@
 import styled, { css } from 'styled-components';
 
-const styles = css`
-  color: var(--white);
-  background: var(--stone-700);
+interface ButtonProps {
+  variant: 'primary' | 'secondary';
+}
+
+const primaryStyles = css`
+  --bg: var(--stone-700);
+  --bg-hover: var(--stone-900);
+  --color: var(--white);
+`;
+
+const secondaryStyles = css`
+  --bg: var(--white);
+  --bg-hover: var(--stone-200);
+  --color: var(--stone-700);
+`;
+
+const styles = css<ButtonProps>`
   padding: var(--size-2) var(--size-4);
   border-radius: var(--radius-sm);
   text-decoration: none;
+  border: 1px solid var(--stone-700);
+  background: var(--bg);
+  color: var(--color);
 
   &:hover {
-    background: var(--stone-900);
+    background: var(--bg-hover);
   }
-`
-export const StyledButton = styled.button`${styles}`;
 
-export const StyledLink = styled.a`${styles}`;
+  ${(props) => (props.variant === 'primary' ? primaryStyles : secondaryStyles)};
+`
+export const StyledButton = styled.button<ButtonProps>`${styles}`;
+
+export const StyledLink = styled.a<ButtonProps>`${styles}`;
